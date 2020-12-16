0cc0c6c205f73d548779187ac9a7409d0ae59cc9
rahman2835 <rahman.sab@servicemax.com>
2020-12-16 11:43:52 +0530

    fix(app): removed old files

--- index.ios.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import {AppRegistry } from 'react-native';
-import SVMXExternalApp from './js/common.js';
-AppRegistry.registerComponent('SVMXExternalApp', () => SVMXExternalApp);
